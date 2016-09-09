31ca1dcda6246870e0398dda6db524b9aeb816f6
Amanda Hinchey & Soren Zeliger & Tyler Sharp <pair+akhinchey.soreasy.tylervsharp@devbootcamp.com>
2016-09-09 16:52:30 -0400

    fixes more stuff

--- funtime_stack.rb
+++ funtime_stack.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class FuntimeStack
 
   attr_accessor :treats, :museums, :parks
@@ -9,9 +11,10 @@ class FuntimeStack
   end
 
   def select_rand
-    treats = [Treat.new('dba' => 'Sorry no place', 'street' => 'We dont exist', 'zipcode' => 'We no exist too')] if treats == nil
-    museums = [Museum.new('name' => 'Sorry no place', 'adress1' => 'We dont exist', 'zip' => 'We no exist too')] if museums == nil
-    parks = [Park.new('signname' => 'Sorry no place', 'location' => 'We dont exist', 'zipcode' => 'We no exist too')] if parks == nil
+    #binding.pry
+    self.treats = [Treat.new('dba' => 'Sorry no place', 'street' => 'We dont exist', 'zipcode' => 'We no exist too')] if treats == []
+    self.museums = [Museum.new('name' => 'Sorry no place', 'adress1' => 'We dont exist', 'zip' => 'We no exist too')] if museums == []
+    self.parks = [Park.new('signname' => 'Sorry no place', 'location' => 'We dont exist', 'zipcode' => 'We no exist too')] if parks == []
     
 
     [museums.sample, parks.sample, treats.sample]
